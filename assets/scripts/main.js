e1dfb4b0984a504c56ab284a55f2ab80ca32dc1d
Nho-way <93555421+Nho-way@users.noreply.github.com>
2023-05-15 22:28:33 -0700

    done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -102,10 +102,16 @@ submit.onclick = function(){
   
 
   // B10. TODO - Get a reference to the "Clear Local Storage" button
+
+  const clear = document.getElementsByClassName('danger')[0];
   // B11. TODO - Add a click event listener to clear local storage button
-  
-  // Steps B12 & B13 will occur inside the event listener from step B11
+  clear.onclick = function(){
+// Steps B12 & B13 will occur inside the event listener from step B11
   // B12. TODO - Clear the local storage
+  localStorage.clear();
   // B13. TODO - Delete the contents of <main>
+  document.querySelector('main').innerHTML = "";
+  }
+  
 
 }
