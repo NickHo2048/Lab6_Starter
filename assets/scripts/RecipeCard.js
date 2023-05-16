002a2680822a991277a3bb41ee9a52736135a62f
Nho-way <93555421+Nho-way@users.noreply.github.com>
2023-05-15 22:09:32 -0700

    explore

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,11 +8,11 @@ class RecipeCard extends HTMLElement {
 
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
-    const shadowDOM = elementRef.attachShadow({ mode: "open" });
+    const shadowDOM = this.attachShadow({ mode: "open" });
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
-    const markup = document.createElement('article');
+    const markup = document.createElement("article");
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-    const style = document.createElement('style');
+    const style = document.createElement("style");
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
     style.textContent = `* {
       font-family: sans-serif;
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,7 +24,11 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
-    
+    if (localStorage.getItem('recipes')){
+      return JSON.parse(localStorage.getItem('recipes'));
+      
+    }
+    else return [];
 }
 
 /**
@@ -36,10 +40,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  const main = document.querySelector("main");
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  for (let i = 0; i < recipes.length; i++){
+    var card = document.createElement("recipe-card");
+    card.data = recipes[i];
+    main.appendChild(card);
+  }
 }
 
 /**
@@ -52,6 +62,7 @@ function saveRecipesToStorage(recipes) {
   // B1. TODO - Complete the functionality as described in this function
   //            header. It is possible in only a single line, but should
   //            be no more than a few lines.
+  localStorage.setItem('recipes', JSON.stringify(recipes));
 }
 
 /**
@@ -61,20 +72,34 @@ function saveRecipesToStorage(recipes) {
 function initFormHandler() {
 
   // B2. TODO - Get a reference to the <form> element
-  
+  const form = document.getElementById("new-recipe");
   // B3. TODO - Add an event listener for the 'submit' event, which fires when the
   //            submit button is clicked
-
+const submit = document.querySelector("button[type='submit']");
+submit.onclick = function(){
   // Steps B4-B9 will occur inside the event listener from step B3
   // B4. TODO - Create a new FormData object from the <form> element reference above
+  const formdata = newFormData(form);
   // B5. TODO - Create an empty object (I'll refer to this object as recipeObject to
   //            make this easier to read), and then extract the keys and corresponding
   //            values from the FormData object and insert them into recipeObject
+  const obj = {};
+  for (let pair of formdata.entries()){
+    obj[pair[0]] = pair[1];
+  }
   // B6. TODO - Create a new <recipe-card> element
+  const nuevo = document.createElement('recipe-card');
   // B7. TODO - Add the recipeObject data to <recipe-card> using element.data
+  nuevo.data = obj;
   // B8. TODO - Append this new <recipe-card> to <main>
+  document.querySelector('main').append(nuevo);
   // B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
   //            then save the recipes array back to localStorage
+  const recipeArr = getRecipesFromStorage();
+  recipeArr.push(obj);
+  saveRecipesToStorage(recipeArr);
+}
+  
 
   // B10. TODO - Get a reference to the "Clear Local Storage" button
   // B11. TODO - Add a click event listener to clear local storage button
